8571abe00f5163343fd07c874750bb22740a8ba4
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-17 23:59:54 +0600

    install tailwind and set google font by next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  weight: ["900", "800", "700", "600", "500", "400", "300", "200", "100"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,46 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold">Hunting Coder</h1>
+      <nav className="p-8 bg-black text-white">
+        <ul className="flex">
+          <li className="mx-5">Home</li>
+          <li className="mx-5">About</li>
+          <li className="mx-5">Blogs</li>
+          <li className="mx-5">Contact</li>
+        </ul>
+      </nav>
+
+      <h1 className="text-center font-bold text-4xl my-6">
+        Super Blog website by HuntingCoders
+      </h1>
+
+      <div className="my-10 mx-10">
+        <h1 className="text-3xl font-bold text-center">Hunting Coder</h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laborum
           beatae odit explicabo assumenda inventore maxime velit eos officiis
           architecto. Aut maiores cupiditate ad quas harum consequuntur ex eaque
           libero!
         </p>
-      </main>
+      </div>
+      <div className="my-5 mx-10">
+        <h1 className="text-3xl font-bold text-center">Hunting Coder</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laborum
+          beatae odit explicabo assumenda inventore maxime velit eos officiis
+          architecto. Aut maiores cupiditate ad quas harum consequuntur ex eaque
+          libero!
+        </p>
+      </div>
+      <div className="my-5 mx-10">
+        <h1 className="text-3xl font-bold text-center">Hunting Coder</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo laborum
+          beatae odit explicabo assumenda inventore maxime velit eos officiis
+          architecto. Aut maiores cupiditate ad quas harum consequuntur ex eaque
+          libero!
+        </p>
+      </div>
     </>
   );
 }
