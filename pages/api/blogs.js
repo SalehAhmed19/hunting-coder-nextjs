1bdd10d595ae07169aa3484b595a4392b515a324
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-18 04:21:05 +0600

    all blogs api created

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,8 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogData", (err, data) => {
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogData");
+  // res.status(200).json(data);
+  let myFile;
+  let allBlogs = [];
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    console.log(item);
+    myFile = await fs.promises.readFile("blogData/" + item);
+    allBlogs.push(JSON.parse(myFile));
+  }
+  res.status(200).json(allBlogs);
 }
