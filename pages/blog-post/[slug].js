325dbf0b15f13e1ce5c7205e1990355de1628caa
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-18 03:02:12 +0600

    make some json data

--- pages/blog-post/[slug].js
+++ pages/blog-post/[slug].js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
 import React from "react";
 
+// step 1: find the file corresponding to the slug
+// step  2: populate them inside the page
+
 const Blog = () => {
   const router = useRouter();
   const { slug } = router.query;
   return (
     <main>
-      <h1 className="text-2xl font-bold">Title of the page: {slug}</h1>
+      <h1 className="text-2xl font-bold">{slug}</h1>
       <hr className="my-5" />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,31 +1,28 @@
 import React from "react";
 import Link from "next/link";
 
+// step 1: collect all data from blogData directory
+// step 2: iterate through them and display them
+
 const Blog = () => {
   return (
     <div>
       <h2 className="text-center font-bold text-4xl my-10">Blogs</h2>
-      <Link href={`/blog-post/learn-javascript`}>
+      <Link href={`/blog-post/Javascript`}>
         <div className="mt-5  bg-[#f9f9f9] p-7 rounded-md">
-          <h3 className="text-xl font-bold mb-2">
-            How to learn JavaScript in 2023?
-          </h3>
+          <h3 className="text-xl font-bold mb-2">About JavaScript</h3>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
       </Link>
-      <Link href={`/blog-post/learn-react`}>
+      <Link href={`/blog-post/React`}>
         <div className="my-5  bg-[#f9f9f9] p-7 rounded-md">
-          <h3 className="text-xl font-bold mb-2">
-            How to learn ReactJs in 2023?
-          </h3>
+          <h3 className="text-xl font-bold mb-2">About React Js</h3>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
       </Link>
-      <Link href={`/blog-post/learn-mui`}>
+      <Link href={`/blog-post/Mui`}>
         <div className="mb-5  bg-[#f9f9f9] p-7 rounded-md">
-          <h3 className="text-xl font-bold mb-2">
-            How to learn Material UI in 2023?
-          </h3>
+          <h3 className="text-xl font-bold mb-2">About Material UI</h3>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
       </Link>
