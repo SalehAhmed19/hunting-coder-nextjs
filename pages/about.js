cde375fb84c2c7225658c32fda668f8a0a3128b3
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-03-19 02:34:00 +0600

    about page updated

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,49 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>This is my about component</h2>
+      <h2 className="text-center text-4xl font-bold mb-5">About us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi amet
+        veritatis, quibusdam deserunt non blanditiis. Velit facere non eveniet
+        suscipit reiciendis consectetur ut deleniti a, sed autem temporibus
+        exercitationem cum? Qui, aliquam ipsum harum sint maiores obcaecati
+        magnam excepturi quibusdam! Recusandae nisi ut tempora aliquam iusto
+        culpa. Error doloribus iure numquam pariatur. Sed soluta eaque,
+        explicabo asperiores a nihil inventore sequi aspernatur beatae, repellat
+        eveniet vitae iste dignissimos enim, facere doloremque dolore
+        repudiandae officia consequuntur fuga accusantium. Eum voluptas
+        provident, accusamus non maiores deleniti pariatur excepturi vel porro
+        error! Voluptate esse similique voluptatum dolore, iste ut dignissimos
+        molestiae rem unde.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi amet
+        veritatis, quibusdam deserunt non blanditiis. Velit facere non eveniet
+        suscipit reiciendis consectetur ut deleniti a, sed autem temporibus
+        exercitationem cum? Qui, aliquam ipsum harum sint maiores obcaecati
+        magnam excepturi quibusdam! Recusandae nisi ut tempora aliquam iusto
+        culpa. Error doloribus iure numquam pariatur. Sed soluta eaque,
+        explicabo asperiores a nihil inventore sequi aspernatur beatae, repellat
+        eveniet vitae iste dignissimos enim, facere doloremque dolore
+        repudiandae officia consequuntur fuga accusantium. Eum voluptas
+        provident, accusamus non maiores deleniti pariatur excepturi vel porro
+        error! Voluptate esse similique voluptatum dolore, iste ut dignissimos
+        molestiae rem unde.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi amet
+        veritatis, quibusdam deserunt non blanditiis. Velit facere non eveniet
+        suscipit reiciendis consectetur ut deleniti a, sed autem temporibus
+        exercitationem cum? Qui, aliquam ipsum harum sint maiores obcaecati
+        magnam excepturi quibusdam! Recusandae nisi ut tempora aliquam iusto
+        culpa. Error doloribus iure numquam pariatur. Sed soluta eaque,
+        explicabo asperiores a nihil inventore sequi aspernatur beatae, repellat
+        eveniet vitae iste dignissimos enim, facere doloremque dolore
+        repudiandae officia consequuntur fuga accusantium. Eum voluptas
+        provident, accusamus non maiores deleniti pariatur excepturi vel porro
+        error! Voluptate esse similique voluptatum dolore, iste ut dignissimos
+        molestiae rem unde.
+      </p>
     </div>
   );
 };
